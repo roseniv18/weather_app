17be76552203a28f89f85ed9123e50ef98c06da2
Avitohol1 <87610719+Avitohol1@users.noreply.github.com>
2022-09-06 16:57:45 +0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,87 @@
-import logo from './logo.svg';
 import './App.css';
+import UilReact from '@iconscout/react-unicons/icons/uil-react'
+import TopButtons from './components/TopButtons'
+import Inputs from './components/Inputs';
+import TimeAndLocation from './components/TimeAndLocation';
+import TemperatureAndDetails from './components/TemperatureAndDetails';
+import Forecast from './components/Forecast';
+import getFormattedWeatherData  from './js/weatherServices';
+import {useState, useEffect} from 'react'
+
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
+
+  const [query, setQuery] = useState({q: 'sofia'})
+  const [units, setUnits] = useState('metric')
+  const [weather, setWeather] = useState(null)
+  
+  useEffect(() => {
+    const fetchWeather = async () => {
+      const message = query.q ? query.q : 'current location.'
+
+      toast.info('Fetching weather for ' + message)
+
+      await getFormattedWeatherData({...query, units}).then(data => {
+
+        toast.success(`Successfully fetched weather for ${data.name}`)
+
+        setWeather(data)
+      })
+    }
+  
+    console.log(weather)
+  
+    fetchWeather()
+  }, [query, units])
+
+  const formatBackground = () => {
+    if(!weather) {
+      return 'from-cyan-700 to-blue-700'
+    }
+
+    const threshold = units === 'metric' ? 20 : 60
+
+    if(weather.temp <= threshold) {
+      return 'from-cyan-700 to-blue-700'
+    }
+
+    return 'from-yellow-700 to-orange-700'
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+    <div 
+      className={`mx-auto 
+                  max-w-screen-md
+                  md:mt-4
+                  sm:mt-0 
+                  py-5 
+                  lg:px-28
+                  sm:px-10
+                  bg-gradient-to-br from-cyan-700 to-blue-700 
+                  h-fit
+                  shadow-xl
+                  shadow-gray-400
+                  ${formatBackground()}`}>
+
+
+    <TopButtons setQuery={setQuery}/>
+    <Inputs setQuery={setQuery} units={units} setUnits={setUnits}/>
+    
+    {weather && (
+      <div>
+        <TimeAndLocation weather = {weather}/>
+        <TemperatureAndDetails units = {units} setUnits = {setUnits} weather = {weather}/>
+        <Forecast title="Hourly Forecast" items={weather.hourly}/>
+        <Forecast title="Daily Forecast" items={weather.daily}/>
+      </div>
+    )}
+
+    <ToastContainer autoClose={3000} theme='colored' newestOnTop={true}/>
+
     </div>
   );
 }
---
+++ src/components/TemperatureAndDetails.js
@@ -0,0 +1,81 @@
+import React from 'react'
+import {
+    UilArrowUp,
+    UilTemperature,
+    UilTear,
+    UilWind,
+    UilSun,
+    UilSunset
+} from '@iconscout/react-unicons'
+import { formatToLocalTime, iconUrlFromCode } from '../js/weatherServices'
+
+function TemperatureAndDetails({weather: {
+    details, icon, temp, temp_min, temp_max, sunrise, sunset, speed, deg, humidity, feels_like, timezone
+}, units})
+    
+{
+    const windUnits = units === 'metric'  ? 'm/s' : 'm/h'
+
+  return (
+    <div>
+        <div className='flex items-center justify-center lg:py-6 sm:py-2 text-xl text-cyan-300 '>
+            <p>{details}</p>
+        </div>
+
+        <div className='flex md:flex-row sm:flex-col items-center justify-between text-white lg:py-3 sm:py-0'>
+            <div className='flex flex-row items-center justify-evenly lg:w-full sm:w-1/2'>
+                <img 
+                    src={iconUrlFromCode(icon)}
+                    alt='weather_icon'
+                    className='w-20' 
+                />
+
+                <p className='text-5xl my-4 sm:my-4'>{`${temp.toFixed()}°`}</p>
+            </div>
+            
+
+            <div className="flex flex-col space-y-2 sm:my-4">
+                <div className='flex font-light text-sm items-center justify-center'>
+                    <UilTemperature size={18} className='mr-1'/>
+                    Feels like:
+                    <span className='font-medium ml-1'>{`${feels_like.toFixed()}°`}</span>
+                </div>
+
+                <div className='flex font-light text-sm items-center justify-center'>
+                    <UilTear size={18} className='mr-1'/>
+                    Humidity:
+                    <span className='font-medium ml-1'>{`${humidity.toFixed()}%`}</span>
+                </div>
+
+                <div className='flex font-light text-sm items-center justify-center'>
+                    <UilWind size={18} className='mr-1'/>
+                    Wind:
+                    <span className='font-medium ml-1'>{`${speed.toFixed()} ${windUnits}`}</span>
+                    <UilArrowUp style={{transform:  `rotate(${deg}deg)`}}/>
+                </div>
+            </div>
+        </div>
+
+        <div className='flex flex-row items-center justify-center space-x-2 text-white text-sm py-3'>
+            <UilSun size={18}/>
+            <p className='font-light'>Rise: <span className='font-medium ml-1'>{formatToLocalTime(sunrise, timezone, 'hh:mm a')}</span></p>
+            <p className='font-light'>|</p>
+            
+            <UilSunset size={18}/>
+            <p className='font-light'>Set: <span className='font-medium ml-1'>{formatToLocalTime(sunset, timezone, 'hh:mm a')}</span></p>
+            <p className='font-light'>|</p>
+
+            <UilTemperature size={18}/>
+            <p className='font-light'>High: <span className='font-medium ml-1'>{`${temp_max.toFixed()}°`}</span></p>
+            <p className='font-light'>|</p>
+
+            <UilTemperature size={18}/>
+            <p className='font-light'>Low: <span className='font-medium ml-1'>{`${temp_min.toFixed()}°`}</span></p>
+            <p className='font-light'>|</p>
+        </div>
+    </div>
+    
+  )
+}
+
+export default TemperatureAndDetails
