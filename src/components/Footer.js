29a0c4a841c2abf6e79684928d3d494ff24136e3
Avitohol1 <87610719+Avitohol1@users.noreply.github.com>
2022-09-08 19:03:19 +0300

    fixed footer translation

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,16 +1,23 @@
 import React from 'react'
 
+// i18Next Translations
+import '../js/translations/i18n'
+import {useTranslation} from 'react-i18next'
+
 function Footer() {
+
+    const { t } = useTranslation()
+
   return (
     <div className='w-100 bg-slate-500 mt-2 text-white py-2 text-center'>
-        Developed By  
+        { t('developed_by') }  
         <span> </span>
         <a 
-            className='text-yellow-500 ease-in-out duration-300 hover:text-yellow-600' 
+            className='text-yellow-500 ease-in-out duration-300 hover:text-yellow-600 footer_name' 
             href='https://github.com/Avitohol1'
             target="_blank"
             rel="noreferrer">
-                 Rosen Ivanov
+                 { t('name') }  
         </a>
     </div>
   )
